20f3a22a14e254d90d9ceaa4582e3ef99d5b2bfb
genvalen <genvalen@protonmail.com>
2021-06-04 05:55:11 -0400

    Make fizzbuzz directory executable

---
+++ fizzbuzz/__main__.py
@@ -0,0 +1,16 @@
+# make fizzbuzz.py executable
+
+import sys
+
+from fizzbuzz import fizz_buzz
+
+args = sys.argv[1:]
+args = map(int, args)
+
+# print output of each argument on a new line
+output = ""
+for arg in args:
+    output += '{}\n'.format(
+        fizz_buzz(arg))
+
+print(output)
--- src/__main__.py
+++
@@ -1,7 +0,0 @@
-# this file makes the fizzbuzz package executable
-
-import sys
-
-from fizzbuzz.fizzbuzz import fizz_buzz
-integer = int(sys.argv[1])
-print(fizz_buzz(integer))
