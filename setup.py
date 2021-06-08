306f73cadca710ec63b2bfe9891024d91319052e
genvalen <genvalen@protonmail.com>
2021-06-08 03:10:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup
 
-setup(name='fizzbuzz',
+with open('README.md', 'r') as fh:
+      long_description= fh.read()
+
+
+setup(name='fizzbuzzer',
       version='0.0.1',
       description='Python package that returns the output of an integer x according to the rules of FizzBuzz.',
+      long_description=long_description, 
+      long_description_content_type='text/markdown',
       url='https://github.com/genvalen/fizzbuzz',
       author='Genvalen',
       author_email='',
       license='MIT',
-      packages=[
-            'fizzbuzz'
-      ],
-      zip_safe=False)
+      packages=['fizzbuzzer'],
+      package_dir={'' : 'src'},
+      zip_safe=False,
+      classifiers=[
+            'Intended Audience :: Education',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.8',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Software Development :: Testing :: Unit'
+      ]
+)
+      
