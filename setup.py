849b32c6f1fb2aa209d59435327fb4b9b346ab16
genvalen <genvalen@protonmail.com>
2021-06-10 17:36:45 -0400

    Update setup files

--- setup.py
+++ setup.py
@@ -6,22 +6,25 @@ with open('README.md', 'r') as fh:
 
 setup(name='fizzbuzzer',
       version='0.0.1',
+      author='Genvalen',
+      author_email='',
       description='Return the output of an integer, x, according to the rules of FizzBuzz on your command line.',
       long_description=long_description, 
       long_description_content_type='text/markdown',
       url='https://github.com/genvalen/fizzbuzz',
-      author='Genvalen',
-      author_email='',
+      project_url= {
+            'Bug Tracker' : 'https://github.com/genvalen/fizzbuzz/issues' 
+      },
       license='MIT',
       packages=['fizzbuzzer'],
       package_dir={'' : 'src'},
-      zip_safe=False,
       classifiers=[
             'Intended Audience :: Education',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.8',
             'License :: OSI Approved :: MIT License',
             'Topic :: Software Development :: Testing :: Unit'
-      ]
+      ],
+      python_requires=">=3.7"
 )
       
